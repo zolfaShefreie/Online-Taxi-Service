d14de235cf635b5c12dc257d45c05e3a441d9229
zolfaShefreie <z.shefreie@gmail.com>
2022-07-01 12:42:10 +0430

    add setting comments

--- settings.py
+++ settings.py
@@ -1,13 +1,16 @@
 import environ
 
 
+# environment setup
 env = environ.Env()
 env_path = "./.env"
 environ.Env.read_env(env_path)
 
+# variables for kafka settings
 BOOTSTRAP_SERVERS = env("BOOTSTRAP_SERVERS", default="localhost:9092")
 RAW_DATA_PATH = env("RAW_DATA_PATH", default=None)
 SORTED_DATA_PATH = env("SORTED_DATA_PATH", default=None)
 
+# validate RAW_DATA_PATH and SORTED_DATA_PATH
 if RAW_DATA_PATH is None and SORTED_DATA_PATH is None:
     raise Exception("GET NO DATA_PATH")
