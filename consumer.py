d321fb543b74e20c31195344793fcc7f132f0ede
FatemehMomeni <50900356+FatemehMomeni@users.noreply.github.com>
2022-07-01 10:13:34 +0430

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -2,7 +2,6 @@ import json
 from kafka import KafkaConsumer
 
 if __name__ == '__main__':
-    # Kafka Consumer
     consumer = KafkaConsumer('data', bootstrap_servers='localhost:9092', auto_offset_reset='earliest')
     for data in consumer:
         print(json.loads(data.value))
