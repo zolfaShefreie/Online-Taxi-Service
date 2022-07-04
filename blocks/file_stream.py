0e49317cba8bfeffe7432c575960c48a7bf57efc
zolfaShefreie <z.shefreie@gmail.com>
2022-07-04 16:58:48 +0430

    changing for when sort file

--- blocks/file_stream.py
+++ blocks/file_stream.py
@@ -2,18 +2,20 @@ from abc import ABC
 import pandas as pd
 import uuid
 import datetime
+import os
 
 from blocks.base_classes import BaseBlock, BlockType
 from settings import RAW_DATA_PATH, SORTED_DATA_PATH
 
 
 class FileStreamBlock(BaseBlock, ABC):
-    FILE_PATH = RAW_DATA_PATH if RAW_DATA_PATH else SORTED_DATA_PATH
+    FILE_PATH = RAW_DATA_PATH if not os.path.exists(SORTED_DATA_PATH) else SORTED_DATA_PATH
     AFTER_BASIC_PROCESS_FILE_PATH = SORTED_DATA_PATH
-    SORT_DATA = True if RAW_DATA_PATH else False
+    SORT_DATA = True if not os.path.exists(SORTED_DATA_PATH) else False
     SORT_BY = 'Date/Time'
     DATETIME_COL = 'Date/Time'
-    DATETIME_FORMAT = "%m/%d/%Y %I:%M:%S %p"
+    # DATETIME_FORMAT = "%m/%d/%Y %I:%M:%S %p"
+    DATETIME_FORMAT = "%m/%d/%Y %H:%M:%S"
 
     def __init__(self, *args, **kwargs):
         super().__init__(consumer_topic=None, block_type=BlockType.normal, consumer_group_id=None, *args, **kwargs)
@@ -24,12 +26,17 @@ class FileStreamBlock(BaseBlock, ABC):
         :param entry_data:
         :return:
         """
-        key = str(uuid.uuid4())
+        key = str.encode(str(uuid.uuid4()))
         element = datetime.datetime.strptime(entry_data[self.DATETIME_COL], self.DATETIME_FORMAT)
-        timestamp = datetime.datetime.timestamp(element)
+        timestamp = int(datetime.datetime.timestamp(element))
         return entry_data.to_dict(), key, timestamp
 
     def _normal_run(self):
+        """
+        run method for normal type
+            create normal
+        :return:
+        """
         self._normal_setup()
         df = pd.read_csv(self.FILE_PATH, header=[0])
         if self.SORT_DATA:
--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 import environ
+import os
 
 
 # environment setup
@@ -9,8 +10,9 @@ environ.Env.read_env(env_path)
 # variables for kafka settings
 BOOTSTRAP_SERVERS = env("BOOTSTRAP_SERVERS", default="localhost:9092")
 RAW_DATA_PATH = env("RAW_DATA_PATH", default=None)
-SORTED_DATA_PATH = env("SORTED_DATA_PATH", default=None)
+SORTED_DATA_PATH = env("SORTED_DATA_PATH", default="sorted_data.csv")
 
 # validate RAW_DATA_PATH and SORTED_DATA_PATH
-if RAW_DATA_PATH is None and SORTED_DATA_PATH is None:
+if (RAW_DATA_PATH is None and not os.path.exists(SORTED_DATA_PATH)) or \
+        (RAW_DATA_PATH is not None and not os.path.exists(RAW_DATA_PATH) and not os.path.exists(SORTED_DATA_PATH)):
     raise Exception("GET NO DATA_PATH")
