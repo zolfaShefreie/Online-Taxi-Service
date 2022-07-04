847827d0e8b41831adac7f29317ec4210e655b82
zolfaShefreie <z.shefreie@gmail.com>
2022-07-04 17:02:55 +0430

    delete extra files

--- consumer.py
+++
@@ -1,7 +0,0 @@
-import json
-from kafka import KafkaConsumer
-
-if __name__ == '__main__':
-    consumer = KafkaConsumer('data', bootstrap_servers='localhost:9092', auto_offset_reset='earliest')
-    for data in consumer:
-        print(json.loads(data.value))
