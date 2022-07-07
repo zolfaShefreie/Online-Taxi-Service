1eeae775aed39d03b9485a2a911f86c1b5849ed9
FatemehMomeni <50900356+FatemehMomeni@users.noreply.github.com>
2022-07-07 09:35:53 +0430

    Update settings.py

--- settings.py
+++ settings.py
@@ -12,6 +12,10 @@ BOOTSTRAP_SERVERS = env("BOOTSTRAP_SERVERS", default="localhost:9092")
 RAW_DATA_PATH = env("RAW_DATA_PATH", default=None)
 SORTED_DATA_PATH = env("SORTED_DATA_PATH", default="sorted_data.csv")
 
+# variables for elasticsearch settings
+ELASTIC_SERVER = env("ELASTIC_SERVER", default="localhost:9200")
+ELASTIC_PASSWORD = env("ELASTIC_PASSWORD", default=None)
+
 # validate RAW_DATA_PATH and SORTED_DATA_PATH
 if (RAW_DATA_PATH is None and not os.path.exists(SORTED_DATA_PATH)) or \
         (RAW_DATA_PATH is not None and not os.path.exists(RAW_DATA_PATH) and not os.path.exists(SORTED_DATA_PATH)):
