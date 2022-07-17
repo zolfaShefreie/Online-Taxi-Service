79c96bb64eb3bff4ec23c397837abd631ae40914
zolfaShefreie <z.shefreie@gmail.com>
2022-07-18 02:35:59 +0430

    add a block for handle online clustering (but it's offline learning)

--- blocks/online_cluster.py
+++ blocks/online_cluster.py
@@ -1,16 +1,14 @@
 from abc import ABC
-from pyspark.mllib.clustering import StreamingKMeans
 from pyspark.sql.types import StructType, StructField, FloatType, StringType
 from pyspark.sql.functions import lit, col
-from pyspark.ml.clustering import KMeansModel
+from pyspark.ml.clustering import KMeansModel, KMeans
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.feature import StandardScaler
-# from pyspark.streaming.kafka import KafkaUtils
+import json
 
 from blocks.base_classes import BaseBlock, BlockType
 from settings import SAVED_TRANSFORMERS_PATH
-
-# from utils.cluster_methods import StructuredStreamingKMeans
+from utils.cluster_methods import OnlineKMeans
 
 
 class PreTrainedClusteringBlock(BaseBlock, ABC):
@@ -19,7 +17,6 @@ class PreTrainedClusteringBlock(BaseBlock, ABC):
 
     def __init__(self, *args, **kwargs):
         super().__init__(block_type=BlockType.spark, *args, **kwargs)
-        # self.cluster_model = StreamingKMeans(k=self.CLUSTER_NUM, timeUnit='points').setRandomCenters(5, 100.0, 42)
         self.assemble = VectorAssembler(inputCols=['Lat', 'Lon'], outputCol='features')
         self.scaler = StandardScaler(inputCol='features', outputCol='standardized')
         self.kmeans_model = KMeansModel.load(self.SAVED_MODEL_PATH)
@@ -64,3 +61,76 @@ class PreTrainedClusteringBlock(BaseBlock, ABC):
         result_df = self._produce_answer(df)
         result_df = result_df.withColumn("Cluster", col("prediction"))
         self._spark_output_setup(result_df)
+
+
+class OnlineClusteringBlock(BaseBlock, ABC):
+    CLUSTER_NUM = 7
+    COlUMN_NAME = ['Lat', 'Lon']
+    ASSEMBLER_OUTPUT = "features"
+    SCALE_OUTPUT = "standardized"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(block_type=BlockType.normal, *args, **kwargs)
+        self.train_data = None
+        self.kmeans_model = KMeans(featuresCol=self.SCALE_OUTPUT, k=self.CLUSTER_NUM)
+        self.assembler = VectorAssembler(inputCols=self.COlUMN_NAME, outputCol=self.ASSEMBLER_OUTPUT)
+        self.scale = StandardScaler(inputCol=self.ASSEMBLER_OUTPUT, outputCol=self.SCALE_OUTPUT)
+
+        # self.cluster_model = OnlineKMeans(k=self.CLUSTER_NUM, timeUnit='points').setRandomCenters(5, 100.0, 42)
+
+    @staticmethod
+    def _get_value_columns():
+        """
+        :return: a list of value columns that used to convert to json
+        """
+        return ['Lat', 'Lon', 'Base', 'Date/Time', 'Cluster']
+
+    def _pre_processed(self, train_dataframe, test_dataframe):
+        """
+        pre_processed dataframes:
+            1.convert to vector
+            2.standard Scaler
+        :param train_dataframe:
+        :param test_dataframe:
+        :return: preprocessed_model
+        """
+        assembled_train_data = self.assembler.transform(train_dataframe)
+        scale_model = self.scale.fit(assembled_train_data)
+        assembled_test_data = self.assembler.transform(test_dataframe)
+        return scale_model.transform(assembled_train_data), scale_model.transform(assembled_test_data)
+
+    def _produce_answer(self, entry_data):
+        """
+        this function do some process on entry_data and return answer for self._send_data arguments
+            1.convert data to rdd
+            2.train model
+            3.predict
+            4.convert to correct format to send to kafka topic
+        :param entry_data:
+        :return:
+        """
+        value = json.loads(entry_data.value.decode('utf-8'))
+        if self.train_data is None:
+            self.train_data = test_df = self.spark_session.createDataFrame([value, ])
+        else:
+            test_df = self.spark_session.createDataFrame([value, ])
+            self.train_data = self.train_data.union(test_df)
+
+        # preprocess and train model to get prediction
+        preprocessed_train_df, preprocessed_test_df = self._pre_processed(self.train_data, test_df)
+        model = self.kmeans_model.fit(preprocessed_train_df)
+        result_df = model.transform(preprocessed_test_df).withColumn("Cluster", col("prediction"))\
+            .select(self._get_value_columns())
+
+        # convert to dictionary
+        value_plus_cluster = list(map(lambda row: row.asDict(), result_df.collect()))
+        return value_plus_cluster[0], entry_data.key, entry_data.timestamp
+
+    def _normal_run(self):
+        self._normal_setup()
+        if self.consumer:
+            for each in self.consumer:
+                produced_data, key, timestamp = self._produce_answer(each)
+                self._send_data(data=produced_data, key=key, timestamp_ms=timestamp)
+        else:
+            pass
--- utils/cluster_methods.py
+++ utils/cluster_methods.py
@@ -1,53 +1,53 @@
 from pyspark.mllib.clustering import StreamingKMeans
-from pyspark.ml.clustering import KMeans
-from pyspark.sql.dataframe import DataFrame
+from pyspark.rdd import RDD
 from pyspark import since
 
 
-class StructuredStreamingKMeans(StreamingKMeans):
+class OnlineKMeans(StreamingKMeans):
+    """
+    rewrite class for rdd-based data
+    """
 
     @since("1.5.0")
-    def trainOn(self, train_dataframe: DataFrame) -> None:
-        """Train the model on the incoming streaming dataframe."""
-        self._validate(train_dataframe)
-
-        def update(x) -> None:
-            rdd = x.rdd
-            self._model.update(rdd, self._decayFactor, self._timeUnit)  # type: ignore[union-attr]
-
-        train_dataframe.writeStream.foreach(update).start()
-
-    def _predict_rdd_string_format(self, x):
-        rdd = x.rdd
-        result = self._model.predict(rdd)
-        return result.zipWithIndex()
+    def trainOn(self, train_data: RDD) -> None:
+        """
+        Train the model on the rdd-based data
+        :param train_data: the format of rdd must be below format
+                (x1, x2, ..., x3) => each element must be a point
+        :return:
+        """
+        self._validate(train_data)
+        self._model.update(train_data, self._decayFactor, self._timeUnit)
 
     @since("1.5.0")
-    def predictOn(self, dataframe: DataFrame, train_columns: list):
+    def predictOn(self, rdd: RDD) -> RDD:
         """
-        Make predictions on a dataframe.
-        Returns a dataframe with cluster column object
+        Make predictions on a rdd.
+        :param rdd: the format of rdd must be below format
+                (x1, x2, ..., x3) => each element must be a point
+        :return:
         """
-        self._validate(dataframe)
-        return dataframe.writeStream.foreach(lambda x: x.withColumn("Cluster",
-                                                                    self._model.predict(x.select(train_columns).rdd)))
-        #return dstream.map(lambda x: self._model.predict(x))  # type: ignore[union-attr]
+        self._validate(rdd)
+        return rdd.map(lambda x: self._model.predict(x))
 
     @since("1.5.0")
-    def predictOnValues(self, dstream: "DStream[Tuple[T, VectorLike]]") -> "DStream[Tuple[T, int]]":
+    def predictOnValues(self, rdd: RDD) -> RDD:
         """
-        Make predictions on a keyed dstream.
-        Returns a transformed dstream object.
+        Make predictions on a keyed rdd.
+        Returns a transformed rdd object.
+        :param rdd: rdd with below format
+                    (key, value) => value = point = (x1, x2, ..., xn)
+        :return: transformed rdd
         """
-        self._validate(dstream)
+        self._validate(rdd)
         return dstream.mapValues(lambda x: self._model.predict(x))  # type: ignore[union-attr]
 
-    def _validate(self, dataframe) -> None:
+    def _validate(self, rdd) -> None:
         if self._model is None:
             raise ValueError(
                 "Initial centers should be set either by setInitialCenters " "or setRandomCenters."
             )
-        if not isinstance(dataframe, DataFrame):
+        if not isinstance(rdd, RDD):
             raise TypeError(
-                "Expected dataframe to be of type DataFrame, " "got type %s" % type(dataframe)
+                "Expected data to be of type RDD, " "got type %s" % type(rdd)
             )
